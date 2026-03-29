9cc696a559da2f510a55e0e47cc9936b4577ca5f
Alen Bogadi <alen.bogadi@sofascore.com>
2026-03-29 19:02:48 +0200

    idea suggestion: public no longer needed on main method in java 25

--- src/main/java/hr/tvz/gaintrack/GaintrackApplication.java
+++ src/main/java/hr/tvz/gaintrack/GaintrackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GaintrackApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(GaintrackApplication.class, args);
 	}
 
