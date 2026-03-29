e97352baaa3a4d51ca5e63a9fa193a242d086d9f
Alen Bogadi <alen.bogadi@sofascore.com>
2026-03-29 12:50:30 +0200

    Initialize project files

---
+++ src/main/java/hr/tvz/gaintrack/GaintrackApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.gaintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaintrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tvz/gaintrack/GaintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.gaintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
