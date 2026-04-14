44c0e371041a0357e32530b98a41b533699eda4e
markopesa <166159978+markopesa@users.noreply.github.com>
2026-04-14 13:08:40 +0200

    GAI-64, GAI-65, GAI-69, GAI-70 - Home page, navbar, create workout allows deletion of sets and exercise (#12)

---
+++ src/main/java/hr/tvz/gaintrack/controller/HomeController.java
@@ -0,0 +1,14 @@
+package hr.tvz.gaintrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showDashboard() {
+        return "dashboard/index";
+    }
+}
+
